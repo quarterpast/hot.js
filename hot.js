85a6324a4e6f3d9b190db1c0add0e123d6cb1e31
Matt Brennan <mattyb1000@gmail.com>
2011-12-13 23:52:44 +0000

    dry, attach module to thingy

--- hot.js
+++ hot.js
@@ -4,14 +4,14 @@ path = require("path");
 exports.modules = {};
 exports.load = function(file) {
 	var that = this;
-	exports.modules[path.basename(file,".js")] = require(file);
-	that.emit("load",exports.modules[path.basename(file,".js")]);
+	this.module = exports.modules[path.basename(file,".js")] = require(file);
+	this.emit("load",this.module);
 	fs.watch(file,function(event,name) {
 		if(!name) name = file;
 		if(name != file) delete exports.modules[path.basename(file,".js")]
 		delete require.cache[path.join(__dirname,name)];
-		exports.modules[path.basename(name,".js")] = require(name);
-		that.emit("reload",exports.modules[path.basename(name,".js")],name);
+		that.module = exports.modules[path.basename(name,".js")] = require(name);
+		that.emit("reload",that.module,name);
 	});
 }
 exports.load.prototype = new process.EventEmitter();
