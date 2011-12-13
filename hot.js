6aaa1bf5abfa004973cae3c104dbc528054f1009
Matt Brennan <mattyb1000@gmail.com>
2011-12-13 23:48:08 +0000

    pass module to callback like a baws

--- hot.js
+++ hot.js
@@ -4,14 +4,14 @@ path = require("path");
 exports.modules = {};
 exports.load = function(file) {
 	var that = this;
-	that.emit("load");
 	exports.modules[path.basename(file,".js")] = require(file);
+	that.emit("load",exports.modules[path.basename(file,".js")]);
 	fs.watch(file,function(event,name) {
 		if(!name) name = file;
 		if(name != file) delete exports.modules[path.basename(file,".js")]
 		delete require.cache[path.join(__dirname,name)];
 		exports.modules[path.basename(name,".js")] = require(name);
-		that.emit("reload",name);
+		that.emit("reload",exports.modules[path.basename(name,".js")],name);
 	});
 }
 exports.load.prototype = new process.EventEmitter();
